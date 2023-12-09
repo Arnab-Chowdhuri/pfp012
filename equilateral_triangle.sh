2eb3a0565f218b3af5e722a52a69a87ad384ef51
Arnab_Chowdhuri <arnabchowdhuri1@gmail.com>
2023-12-10 01:37:12 +0530

    Equilateral_Triangle is added successfully

---
+++ equilateral_triangle.sh
@@ -0,0 +1,14 @@
+read -p "Enter the size of equilateral angle triangle : " a
+j=1
+for ((i=1;i<=a;i++))
+do
+    for ((s=a-1;s>=i;s--))
+    do
+        echo -n "  "
+    done
+    for ((j=1;j<=2*i-1;j++))
+    do
+        echo -n "* "
+    done
+    echo
+done
